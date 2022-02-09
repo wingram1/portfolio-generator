a89e6fab241cfd04866eae1108871328487cc57f
wingram1 <waingram96@gmail.com>
2022-02-09 14:02:08 -0600

    added updated template

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -137,13 +137,11 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-      console.log(portfolioData);
+      const pageHTML = generatePage(portfolioData);
+
+      fs.writeFile('./index.html', pageHTML, err => {
+          if (err) throw new Error (err);
+
+          console.log('Page created! Check out index.html to see the output!')
+      });
     });
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw new Error (err);
-
-//     console.log('Portfolio complete! Check out index.html to see the output!')
-// });
