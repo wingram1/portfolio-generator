5a58901cfe2f29e24531666aaff65b659d65f291
wingram1 <waingram96@gmail.com>
2022-02-09 18:41:02 -0600

    added stylesheet, made dist and util folders, and added function to copy files from src to dist

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site')
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -135,13 +135,22 @@ const promptProject = portfolioData => {
   };
 
 promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-      const pageHTML = generatePage(portfolioData);
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
-      fs.writeFile('./index.html', pageHTML, err => {
-          if (err) throw new Error (err);
 
-          console.log('Page created! Check out index.html to see the output!')
-      });
-    });
